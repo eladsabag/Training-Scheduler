3d8f2435fb11ce78eecb08783661818c7c3634e5
Elad Sabag <Elad.Sabag@s.afeka.ac.il>
2022-03-02 13:59:16 +0200

    Update files

--- app.js
+++ app.js
@@ -256,9 +256,9 @@ app.get("/contact", function(req,res) {
 });
 
 let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+// if (port == null || port == "") {
+//   port = 3000;
+// }
 
 app.listen(port, function() {
   console.log("Server started successfully.");
